5a9be52f8817091d4f179e9365efb8b9e1a36d96
Omerhrr <pyomerhrr@gmail.com>
2023-03-15 12:29:17 +0100

    newest commit

--- build_files.sh
+++ build_files.sh
@@ -6,9 +6,9 @@ python3.11.1 -m pip install -r requirements.txt
 
 
 echo "Making Migrations"
-python3.11.1 manage.py makemigrations --noinput 
+python manage.py makemigrations --noinput 
 
-python3.11.1 manage.py migrate --noinput 
+python manage.py migrate --noinput 
 
 echo "Collect Static..."
-python3.11.1 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
