95062962baff800e61857a6b13a6894e99086bc2
Omerhrr <pyomerhrr@gmail.com>
2023-03-15 12:18:21 +0100

    python correct version added

--- build_files.sh
+++ build_files.sh
@@ -2,13 +2,13 @@
 echo "Building new project"
 echo "Installing Requirements"
 
-python3.11 -m pip install -r requirements.txt 
+python3.11.1 -m pip install -r requirements.txt 
 
 
 echo "Making Migrations"
-python3.11 manage.py makemigrations --noinput 
+python3.11.1 manage.py makemigrations --noinput 
 
-python3.11 manage.py migrate --noinput 
+python3.11.1 manage.py migrate --noinput 
 
 echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+python3.11.1 manage.py collectstatic --noinput --clear
